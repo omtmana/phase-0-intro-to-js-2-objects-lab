b50fa30d410837fc383c0df1e05da72f58e4da8b
Olivia <oliviatmanalastas@gmail.com>
2022-06-13 12:05:03 -0400

    commit tree 93c775ccac65c95dbd11cbdba32c97a6d97bf002

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+let employee = {
+    name: "Olivia Molly" ,
+    streetAddress: "1864 Sagu St.",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let newEmployeeObject = {...employee};
+    newEmployeeObject[key] = value;
+    return newEmployeeObject;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey (employee, key) {
+    let newEmployeeByKey = {...employee};
+    delete newEmployeeByKey[key];
+    return newEmployeeByKey;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
